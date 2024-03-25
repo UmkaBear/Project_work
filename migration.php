c1280eccd518170b308a1c6599c9b85d6a17cdde
UmkaBear <dfakhiev@yandex.ru>
2024-03-25 17:20:47 +0500

    Исправление ошибок migration.php

--- migration.php
+++ migration.php
@@ -36,10 +36,10 @@ require(__DIR__.'/../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$mysqldb = $_ENV["DB_HOST"];
-$mysqldb_username = $_ENV["DB_LOGIN"];
-$pass = $_ENV["DB_PASSWORD"];
-$database = $_ENV["DB_BASE_NAME"];
+$dbHost = $_ENV["DB_HOST"];
+$dbUsername = $_ENV["DB_LOGIN"];
+$dbPassword = $_ENV["DB_PASSWORD"];
+$dbName = $_ENV["DB_BASE_NAME"];
 $filePath = 'sql/project_1.sql';
 
-restoreDatabaseTables($mysqldb, $mysqldb_username, $pass, $database, $filePath);
+restoreDatabaseTables($dbHost, $dbUsername, $dbPassword, $dbName, $filePath);
