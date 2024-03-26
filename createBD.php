8cdec6c5f0823440be20151873bef2db315ef5fc
UmkaBear <dfakhiev@yandex.ru>
2024-03-26 11:48:51 +0500

    refactor|Перенес все обращения к БД в function.php из скриптов создания и переноса БД

--- createBD.php
+++ createBD.php
@@ -1,23 +1,7 @@
 <?php
+require_once "src/function.php";
 require(__DIR__.'/../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$mysqldb = $_ENV["DB_HOST"];
-$mysqldb_username = $_ENV["DB_LOGIN"];
-$pass = $_ENV["DB_PASSWORD"];
-
-$conn = new mysqli($mysqldb, $mysqldb_username, $pass);
-
-if ($conn->connect_error) {
-    die("Ошибка" . $conn->connect_error);
-}
-
-$sql = "CREATE DATABASE test";
-if ($conn->query($sql) === TRUE) {
-    echo "Создано";
-} else {
-    echo "Ошибка" . $conn->error;
-}
-
-$conn->close();
+createDatabase();
--- migration.php
+++ migration.php
@@ -1,37 +1,5 @@
 <?php
-function restoreDatabaseTables($dbHost, $dbUsername, $dbPassword, $dbName, $filePath){
-
-    $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-
-    $templine = '';
-
-
-    $lines = file($filePath);
-
-    $error = '';
-
-
-    foreach ($lines as $line){
-
-        if(substr($line, 0, 2) == '--' || $line == ''){
-            continue;
-        }
-
-        $templine .= $line;
-
-
-        if (substr(trim($line), -1, 1) == ';'){
-
-            if(!$db->query($templine)){
-                $error .= 'Ошибка<b>' .
-                    $templine . '</b>": ' . $db->error . '<br /><br />'; }
-
-
-            $templine = '';
-        }
-    }
-    return !empty($error)?$error:true;
-}
+require_once "src/function.php";
 require(__DIR__.'/../vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
